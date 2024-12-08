9e5c979e3c9c5a577bde9c8d8eca25f847e290cc
Damilola <damilolaoni07@gmail.com>
2024-12-08 07:59:48 +0100

    responsiveness completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className="footer-bg bg-[#272d4f] h-[8%] w-full flex justify-center items-center">
+    <div className="footer-bg mt-4 bg-[#272d4f] h-[8%] w-full flex justify-center items-center">
         <h1 className="text-[1.2rem] text-white">copyright 2025</h1>
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="h-[12%] header-bg w-full flex justify-center items-center relative" style={{backgroundImage: `url(${des})`}}>
       <div className="absolute inset-0 bg-[rgb(2,0,36)] opacity-75"></div>
-      <h1 className="text-[2.05rem] text-white font-bold z-10">D'Oracle Cipher Machine</h1>
+      <h1 className="text-[1.3rem] sm:text-[2.05rem] text-white font-bold z-10">D'Oracle Cipher Machine</h1>
     </div>
   )
 }
