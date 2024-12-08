6d92a5c231f28c0da74bddbf096d5b479ee79a2e
Damilola <damilolaoni07@gmail.com>
2024-12-08 08:05:50 +0100

    footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@
 
 const Footer = () => {
   return (
-    <div className="footer-bg mt-4 bg-[#272d4f] h-[8%] w-full flex justify-center items-center">
-        <h1 className="text-[1.2rem] text-white">copyright 2025</h1>
+    <div className="footer-bg mt-6 sm:mt-0 bg-[#272d4f] h-[8%] w-full flex justify-center items-center">
+        <h1 className="text-[0.9rem] sm:text-[1.2rem] text-white">copyright © 2024</h1>
     </div>
   )
 }
